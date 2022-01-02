140e6486651f7f457c65cc083bcfe4a7b020ae4d
Felix Bell <felix@andre-bell.de>
2022-01-02 16:50:50 +0100

    no skam

--- You are awesome/ViewController.swift
+++ You are awesome/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    var ersterclick: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("👍🏻 viewDidload has run!")
+        messageLabel.text = "no skam"
+        ersterclick = true
     }
 
-
+    @IBAction func massageButtenPress(_ sender: UIButton) {
+        if ersterclick {
+            print("😎hi🤩")
+            messageLabel.text = "klick again"
+            ersterclick = false
+        } else {
+            messageLabel.text = "U R Awesome!"
+        }
+    }
+    
 }
 
